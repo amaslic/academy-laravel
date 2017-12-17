6fa2964ea800448973443ba3fbf6678a990cca1a
Vitaliy Makovyak <thisinfoms@gmail.com>
2017-12-18 01:24:41 +0200

    fix(app): Login link moved to center

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -70,9 +70,6 @@
                 <div class="top-right links">
                     @if (Auth::check())
                         <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        {{--<a href="{{ url('/register') }}">Register</a>--}}
                     @endif
                 </div>
             @endif
@@ -84,12 +81,14 @@
 
                 <div class="links">
                     <a href="{{ route('kkic') }}">KKIC</a>
-
                 </div>
 
-                  <div class="links">
+                <div class="links">
                     <a href="{{ route('invites') }}">All Invites</a>
-
+                </div>
+                
+                <div class="links">
+                    <a href="{{ route('login') }}">Login</a>
                 </div>
             </div>
         </div>
