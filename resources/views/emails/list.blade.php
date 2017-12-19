351956aeb0a87a0be440b9431b3a62526ec9c12f
Vitaliy Makovyak <thisinfoms@gmail.com>
2017-12-20 01:31:42 +0200

    Email template

--- resources/views/emails/list.blade.php
+++ resources/views/emails/list.blade.php
@@ -1,16 +1,27 @@
-I recently joined a training program that helped me to to restart my marketing business and improve my life for the better.
-<br><br>
-I can invitee three people and I chose you as one of them.
-<br><br>
-If you join using my invitation you will receive a massive $100 discount.
-<br><br>
-Like I said when we chatted, the program changed my life and I think it
-<br><br>
-might do the same for you... What if, right?
-<br><br>
-Click the button below to accept my invitation and learn more..
-<br><br>
-<a href="{{ url('/kkic/invitation') }}/{{ $id }}" style="
+<table width="100%" bgcolor="#ffffff" border="0" cellpadding="10" cellspacing="0">
+    <tr>
+        <td>
+            <!--[if (gte mso 9)|(IE)]>
+            <table width="600" align="center" cellpadding="0" cellspacing="0" border="0">
+                <tr>
+                    <td>
+            <![endif]-->
+            <table bgcolor="#ffffff" class="content" align="center" cellpadding="0" cellspacing="0" border="0">
+                <tr>
+                    <td valign="top" mc:edit="headerBrand" id="templateContainerHeader">
+                        <h3 style="text-align: center !important;">KKIC Refer-A-Friend Program</h3>
+                        <h4 style="text-align: center !important;">You are receiving One of only Three Invites I have...</h4>
+                        <h6 style="text-align:center;margin:0;padding:0;"><a href="VIEW IN BROWSER URL HERE" style="color:#3F4454">Is this email difficult to read? Click here to view it in your browser</a></h6>
+                    </td>
+                </tr>
+                <tr>
+                    <td align="center" valign="top">
+                        <!-- BEGIN BODY // -->
+                        <table border="0" cellpadding="0" cellspacing="0" width="100%" id="templateContainer">
+                            <tr>
+                                <td valign="top" class="bodyContent" mc:edit="body_content">
+                                    EMAIL CONTENT GOES HERE<br><br>
+                                    <a href="{{ url('/kkic/invitation') }}/{{ $id }}" style="
     background: green;
     color: white;
     padding: 10px 23px;
@@ -19,3 +30,332 @@ Click the button below to accept my invitation and learn more..
     text-align: center;
     display: block;
 ">Accept Invite & Learn More</a>
+                                </td>
+                            </tr>
+                        </table>
+                        <!-- // END BODY -->
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <h6 style="text-align:center;margin-top: 9px;">PUT FOOTER CONTENT HERE</h6>
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+
+</table>
+<!--[if (gte mso 9)|(IE)]>
+</td>
+</tr>
+</table>
+<![endif]-->
+</td>
+</tr>
+</table>
+<style type="text/css">
+    /* /\/\/\/\/\/\/\/\/ CLIENT-SPECIFIC STYLES /\/\/\/\/\/\/\/\/ */
+    #outlook a{padding:0;} /* Force Outlook to provide a "view in browser" message */
+    .ReadMsgBody{width:100%;} .ExternalClass{width:100%;} /* Force Hotmail to display emails at full width */
+    .ExternalClass, .ExternalClass p, .ExternalClass span, .ExternalClass font, .ExternalClass td, .ExternalClass div {line-height: 100%;} /* Force Hotmail to display normal line spacing */
+    body, table, td, p, a, li, blockquote{-webkit-text-size-adjust:100%; -ms-text-size-adjust:100%;} /* Prevent WebKit and Windows mobile changing default text sizes */
+    table, td{mso-table-lspace:0pt; mso-table-rspace:0pt;} /* Remove spacing between tables in Outlook 2007 and up */
+    img{-ms-interpolation-mode:bicubic;max-width:100%;} /* Allow smoother rendering of resized image in Internet Explorer */
+    td ul li {
+        font-size: 16px;
+    }
+
+    body{margin:0; padding:0;}
+    img{
+        max-width:100%;
+        border:0;
+        line-height:100%;
+        outline:none;
+        text-decoration:none;
+    }
+    table{border-collapse:collapse !important;}
+    .content {width: 100%; max-width: 600px;}
+    .content img { height: auto; min-height: 1px; }
+    .content {width: 100%; max-width: 600px;}
+
+    #bodyTable{margin:0; padding:0; width:100% !important;}
+    #bodyCell{margin:0; padding:0;}
+    #bodyCellFooter{margin:0; padding:0; width:100% !important;padding-top:39px;padding-bottom:15px;}
+    body {margin: 0; padding: 0; min-width: 100%!important;}
+
+    #templateContainerHeader{
+        font-size: 14px;
+        padding-top:2.429em;
+        padding-bottom:0.929em;
+    }
+    #templateContainerFootBrd {
+        border-bottom:1px solid #e2e2e2;
+        border-left:1px solid #e2e2e2;
+        border-right:1px solid #e2e2e2;
+        border-radius: 0 0 4px 4px;
+        background-clip: padding-box;
+        border-spacing: 0;
+        height: 10px;
+        width:100% !important;
+    }
+    #templateContainer{
+        border-top:1px solid #e2e2e2;
+        border-bottom:1px solid #e2e2e2;
+        border-left:1px solid #e2e2e2;
+        border-right:1px solid #e2e2e2;
+        border-radius: 4px 4px 4px 4px;
+        background-clip: padding-box;
+        border-spacing: 0;
+    }
+
+    h1{
+        color:#333333;
+        display:block;
+        font-family:Helvetica;
+        font-size:26px;
+        line-height:1.385em;
+        font-style:normal;
+        font-weight:700;
+        letter-spacing:normal;
+        margin-top:0;
+        margin-right:0;
+        margin-bottom:15px;
+        margin-left:0;
+        text-align:left;
+    }
+
+    h2{
+        color:#333333;
+        display:block;
+        font-family:Helvetica;
+        font-size:22px;
+        line-height:1.455em;
+        font-style:normal;
+        font-weight:700;
+        letter-spacing:normal;
+        margin-top:0;
+        margin-right:0;
+        margin-bottom:15px;
+        margin-left:0;
+        text-align:left;
+    }
+
+    h3{
+        color:#333333;
+        display:block;
+        font-family:Helvetica;
+        font-size:18px;
+        line-height:1.444em;
+        font-style:normal;
+        font-weight:700;
+        letter-spacing:normal;
+        margin-top:0;
+        margin-right:0;
+        margin-bottom:15px;
+        margin-left:0;
+        text-align:left;
+    }
+
+    h4{
+        color:#333333;
+        display:block;
+        font-family:Helvetica;
+        font-size:14px;
+        line-height:1.571em;
+        font-style:normal;
+        font-weight:700;
+        letter-spacing:normal;
+        margin-top:0;
+        margin-right:0;
+        margin-bottom:15px;
+        margin-left:0;
+        text-align:left;
+    }
+
+
+    h5{
+        color:#333333;
+        display:block;
+        font-family:Helvetica;
+        font-size:13px;
+        line-height:1.538em;
+        font-style:normal;
+        font-weight:700;
+        letter-spacing:normal;
+        margin-top:0;
+        margin-right:0;
+        margin-bottom:15px;
+        margin-left:0;
+        text-align:left;
+    }
+
+
+    h6{
+        color:#333333;
+        display:block;
+        font-family:Helvetica;
+        font-size:12px;
+        line-height:2.000em;
+        font-style:normal;
+        font-weight:normal;
+        letter-spacing:normal;
+        margin-top:0;
+        margin-right:0;
+        margin-bottom:15px;
+        margin-left:0;
+        text-align:left;
+    }
+
+    p {
+        color:#505050;
+        display:block;
+        font-family:Helvetica;
+        font-size:20px;
+        line-height:1.500em;
+        font-style:normal;
+        font-weight:normal;
+        letter-spacing:normal;
+        margin-top:0;
+        margin-right:0;
+        margin-bottom:15px;
+        margin-left:0;
+        text-align:left;
+    }
+
+    .unSubContent a:visited { color:  #E15A00; text-decoration:underline; font-weight:normal;}
+    .unSubContent a:focus   { color:  #E15A00; text-decoration:underline; font-weight:normal;}
+    .unSubContent a:hover   { color:  #E15A00; text-decoration:underline; font-weight:normal;}
+    .unSubContent a:link   { color: #E15A00 ; text-decoration:underline; font-weight:normal;}
+    .unSubContent a .yshortcuts   { color: #E15A00; text-decoration:underline; font-weight:normal;}
+
+    .unSubContent h6 {
+        color: #808080;
+        font-size: 12px;
+        line-height: 1.5em;
+        margin-bottom: 0;
+    }
+
+    .bodyContent{
+        color:#505050;
+        font-family:Helvetica;
+        font-size:14px;
+        line-height:150%;
+        padding-top:3.143em;
+        padding-right:3.5em;
+        padding-left:3.5em;
+        padding-bottom:3.143em;
+        text-align:left;
+    }
+    .bodyContentImage {
+        color:#505050;
+        font-family:Helvetica;
+        font-size:14px;
+        line-height:150%;
+        padding-top:0;
+        padding-right:3.571em;
+        padding-left:3.571em;
+        padding-bottom:2em;
+        text-align:left;
+    }
+
+    .bodyContentImage h4 {
+        color: #828282;
+        font-size: 13px;
+        line-height: 1.154em;
+        font-weight:normal;
+        margin-bottom: 0;
+    }
+    .bodyContentImage h5 {
+        color: #828282;
+        font-size: 12px;
+        line-height: 1.667em;
+        margin-bottom: 0;
+    }
+
+
+    a:visited { color: #E15A00; text-decoration:none;}
+    a:focus   { color: #E15A00; text-decoration:none;}
+    a:hover   { color: #E15A00; text-decoration:none;}
+    a:link   { color: #E15A00; text-decoration:none;}
+    a .yshortcuts   { color:  #E15A00 ; text-decoration:none;}
+
+    .bodyContent img{
+        height:auto;
+        max-width:498px;
+    }
+
+    .footerContent{
+        color:#808080;
+        font-family:Helvetica;
+        font-size:10px;
+        line-height:150%;
+        padding-top:2.000em;
+        padding-right:2.000em;
+        padding-bottom:2.000em;
+        padding-left:2.000em;
+        text-align:left;
+    }
+
+    .footerContent a:link, .footerContent a:visited, /* Yahoo! Mail Override */ .footerContent a .yshortcuts, .footerContent a span /* Yahoo! Mail Override */{
+        color:#E15A00;
+        font-weight:normal;
+        text-decoration:underline;
+    }
+
+
+    @media only screen and (max-width: 550px), screen and (max-device-width: 550px) {
+        body[yahoo] .hide {display: none!important;}
+        body[yahoo] .buttonwrapper {background-color: transparent!important;}
+        body[yahoo] .button {padding: 0px!important;}
+        body[yahoo] .button a {background-color: #E15A00; padding: 15px 15px 13px!important;}
+        body[yahoo] .unsubscribe {display: block; margin-top: 20px; padding: 10px 50px; background: rgb(255,172,43); border-radius: 5px; text-decoration: none!important; font-weight: normal;}
+    }
+
+    @media only screen and (max-width: 480px){
+        h1 {
+            font-size:34px !important;
+        }
+        h2{
+            font-size:30px !important;
+        }
+        h3{
+            font-size:24px !important;
+        }
+        h4{
+            font-size:18px !important;
+        }
+        h5{
+            font-size:16px !important;
+        }
+        h6{
+            font-size:14px !important;
+        }
+        p {
+            font-size: 18px !important;
+        }
+
+        .bodyContent {
+            padding: 6% 5% 1% 6% !important;
+        }
+        .bodyContent img {
+            max-width: 100% !important;
+        }
+        .bodyContentImage {
+            padding: 3% 6% 3% 6% !important;
+        }
+        .bodyContentImage img {
+            max-width: 100% !important;
+        }
+        .bodyContentImage h4 {
+            font-size: 16px !important;
+        }
+        .bodyContentImage h5 {
+            font-size: 15px !important;
+            margin-top:0;
+        }
+    }
+    .ii a[href] {color: inherit !important;}
+    span > a, span > a[href] {color: inherit !important;}
+    a > span, .ii a[href] > span {text-decoration: inherit !important;}
+</style>
