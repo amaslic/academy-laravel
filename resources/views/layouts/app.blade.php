ddb767d7c2d99835c013402f2fb9b8c1c1d73f82
webroot <vershinin.web@gmail.com>
2017-12-22 14:49:43 +0200

    wireframe 3

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,7 +33,7 @@
                     </a>
                 </div>
 
-                <li class="collapse navbar-collapse" id="app-navbar-collapse">
+                <div class="collapse navbar-collapse" id="app-navbar-collapse">
                     <!-- Left Side Of Navbar -->
                     <ul class="nav navbar-nav">
                         &nbsp;
